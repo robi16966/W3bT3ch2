e6151d1c684b31d5d243843fdf25d111861e7264
xmolnarv <viktoria.molnar95@gmail.com>
2017-05-22 12:25:53 +0200

    zakladny php subor
    obsahuje header, menu a footer... tam kde je text "TEXT SEM!!" pridajte svoj vlastny kod aby kazda stranka vyzerala rovnako.

---
+++ zadanieFinal1/ZAKLAD.php
@@ -0,0 +1,23 @@
+<?php include_once 'lang/lang.php';?>
+<!DOCTYPE HTML>
+<html lang="sk">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title><?php echo $lang['PAGE_TITLE']; ?></title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/styly.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<?php include_once ('header/header.php');
+include_once ('menu/menu.php');
+?>
+
+------ TEXT SEM!!! -----
+
+<br><br><br><br>
+<?php include_once 'footer/footer.php';?>
+</body>
