de6c78902b32eb3bdbbba56ff912f3f0eab58ccb
xmolnarv <viktoria.molnar95@gmail.com>
2017-05-25 09:37:04 +0200

    header updated

--- zadanieFinal1/header/header.php
+++ zadanieFinal1/header/header.php
@@ -1,13 +1,17 @@
 <header>
-<h2><?php echo $lang['PAGE_TITLE']; ?></h2>
+<img src="header/logo.png" align="left"> 
+<h3 class="aumt">&emsp;<?php echo $lang['HEADER_AUMT']; ?></h3>
+
+<div id="lang">
 <?php
 if(strpos($_SERVER['REQUEST_URI'], '?' ) !== false ){
-    echo '<a href="'.$_SERVER['REQUEST_URI'] .'&lang=en">English</a>';
-    echo '<a href="'.$_SERVER['REQUEST_URI'] .'&lang=sk">Slovensky</a>';
+    echo '<a href="'.$_SERVER['REQUEST_URI'] .'&lang=en"><img src="header/english.png"></a>';
+    echo '<a href="'.$_SERVER['REQUEST_URI'] .'&lang=sk"><img src="header/slovakia-flag.gif"></a>';
 }
 else{
-        echo '<a href="'.$_SERVER['REQUEST_URI'] .'?lang=en">English</a>';
-    echo '<a href="'.$_SERVER['REQUEST_URI'] .'?lang=sk">Slovensky</a>';
+        echo '<a href="'.$_SERVER['REQUEST_URI'] .'?lang=en"><img src="header/english.png"></a>';
+    echo '<a href="'.$_SERVER['REQUEST_URI'] .'?lang=sk"><img src="header/slovakia-flag.gif"></a>';
 }
 ?>
+</div>
 </header>
